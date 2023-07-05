b91fb436e009f7145c3ef4a2181390bc598dfe37
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2023-07-05 12:55:35 +0900

    PhysBone

---
+++ Editor/PhysBone.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+namespace gomoru.su.CopyComponentUtilities
+{
+    public static class PhysBone
+    {
+        [MenuItem("CONTEXT/VRCPhysBone/Copy Component to Clipboard")]
+        public static void Copy(MenuCommand menuCommand)
+        {
+            var target = menuCommand.context as VRCPhysBone;
+            var json = JsonUtility.ToJson(target);
+            GUIUtility.systemCopyBuffer = json;
+        }
+
+        [MenuItem("CONTEXT/VRCPhysBone/Paste Component from Clipboard")]
+        public static void Paste(MenuCommand menuCommand)
+        {
+            var target = menuCommand.context as VRCPhysBone;
+            var json = GUIUtility.systemCopyBuffer;
+            JsonUtility.FromJsonOverwrite(json, target);
+        }
+
+        [MenuItem("CONTEXT/VRCPhysBone/Paste Component from Clipboard", validate = true)]
+        public static bool CanPaste()
+        {
+            var obj = new GameObject() { hideFlags = HideFlags.HideInHierarchy };
+            try
+            {
+                var pb = obj.AddComponent<VRCPhysBone>();
+                JsonUtility.FromJsonOverwrite(GUIUtility.systemCopyBuffer, pb);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                GameObject.DestroyImmediate(obj);
+            }
+        }
+    }
+}
